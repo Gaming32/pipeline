f64a908d64d4bdc40679ba06b50f890cc38afc2b
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2022-01-23 06:59:33 -0600

    [no ci] Remove everything

--- src/main/java/io/github/gaming32/pipeline/iteratorbuilder/RunStatement.java
+++
@@ -1,9 +0,0 @@
-package io.github.gaming32.pipeline.iteratorbuilder;
-
-class RunStatement<E> implements Statement<E> {
-    final Runnable func;
-
-    public RunStatement(Runnable func) {
-        this.func = func;
-    }
-}
--- src/main/java/io/github/gaming32/pipeline/iteratorbuilder/Statement.java
+++
@@ -1,4 +0,0 @@
-package io.github.gaming32.pipeline.iteratorbuilder;
-
-interface Statement<E> {
-}
